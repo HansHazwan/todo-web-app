c388dd7c1982b807ab738d21beeaea11cfc99b6c
HansHazwan <muhammadhazwanzufar79@gmail.com>
2024-11-14 19:54:52 +0800

    Adding thiserror Library

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ mod error;
 
 use crate::error::Error;
 use dotenvy::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenv().ok();
+
+    let port = env::var("PORT").unwrap();
+    let address = env::var("ADDRESS").unwrap();
+
+    println!("PORT: {}, ADDRESS: {}", port, address);
 
     Ok(())
 }
